a8f2d5e1fc650f5e67083824129265efa7878f54
asdasd <kolyachepalov037@gmail.com>
2024-12-23 01:11:39 +0900

    Сделал третий экран
    Использовал useEffect

--- chepalovNikolay/App.js
+++ chepalovNikolay/App.js
@@ -4,7 +4,7 @@ const Tab = createBottomTabNavigator();
 
 import Lab1 from "./screens/lab1";
 import Lab2 from "./screens/lab2";
-// import Lab3 from "./screens/lab3";
+import Lab3 from "./screens/lab3";
 import { NavigationContainer } from "@react-navigation/native";
 const colors = ["black", "red", "yellow"];
 
@@ -14,7 +14,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Lab1" component={Lab1} />
         <Tab.Screen name="Lab2" component={Lab2} />
-        {/* <Tab.Screen name="Lab3" component={Lab3} /> */}
+        <Tab.Screen name="Lab3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
