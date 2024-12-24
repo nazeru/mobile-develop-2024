8cb5b1ed2adbc38e11a675e9ca160321b445ab14
Aisen Sleptsov <aisenviperr@mail.ru>
2024-12-24 22:43:05 +0900

    Добавил компонент с useMemo. Поменял название лаб2,3 на лаб1,2

--- aisenSleptsov/slaian/layout/lab2.js
+++ aisenSleptsov/slaian/layout/lab2.js
@@ -1,51 +1,59 @@
 import React, { useState } from "react";
-import { SafeAreaView, Text, TouchableOpacity, View, StyleSheet } from "react-native";
-
-const colors = ["black", "red", "yellow", "green", "gray", "blue"];
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 
 const Lab2 = () => {
-  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
+  const [country, setCountry] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const getRandomColor = () => {
-    let newColor;
-    do {
-      newColor = colors[Math.floor(Math.random() * colors.length)];
-    } while (newColor === backgroundColor);
-    return newColor;
-  };
-
-  const changeColor = () => {
-    setBackgroundColor(getRandomColor());
+  const getCountry = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all");
+      const data = await response.json();
+      const randomCountry =
+        data[Math.floor(Math.random() * data.length)].name.common;
+      setCountry(randomCountry);
+    } catch (error) {
+      console.error("Error fetching country:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor }]}>
-      <View style={styles.innerContainer}>
-        <TouchableOpacity onPress={changeColor} style={styles.button}>
-          <Text style={styles.buttonText}>Нажми!</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Random Country:</Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Text style={styles.text}>{country}</Text>
+      )}
+      <Button title="Get Country" onPress={getCountry} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  innerContainer: {
-    flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#f5f5f5",
   },
-  button: {
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
-    marginTop: 10,
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
   },
-  buttonText: {
-    color: "gray",
+  text: {
+    fontSize: 18,
+    marginVertical: 15,
   },
 });
 
--- aisenSleptsov/slaian/layout/lab3.js
+++ aisenSleptsov/slaian/layout/lab3.js
@@ -1,49 +1,35 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import React, { useState, useMemo } from "react";
+import { View, Text, FlatList, Button } from "react-native";
 
-const Lab3 = () => {
-  const [country, setCountry] = useState('');
-  const [loading, setLoading] = useState(false);
+const DATA = Array.from({ length: 21 }, (_, i) => ({
+  id: i,
+  name: `Компонент ${i}`,
+}));
 
-  const getCountry = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch('https://restcountries.com/v3.1/all');
-      const data = await response.json();
-      const randomCountry = data[Math.floor(Math.random() * data.length)].name.common;
-      setCountry(randomCountry);
-    } catch (error) {
-      console.error('Error fetching country:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+const Item = ({ name }) => {
+  return <Text>{name}</Text>;
+};
+
+const MemoizedItem = React.memo(Item);
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const memoizedData = useMemo(() => {
+    return DATA.map((item) => ({ ...item, name: `${item.name} ${count}` }));
+  }, [count]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Random Country:</Text>
-      {loading ? <ActivityIndicator size="large" color="#0000ff" /> : <Text style={styles.text}>{country}</Text>}
-      <Button title="Get Country" onPress={getCountry} />
+    <View>
+      <Text>СЧЕТ: {count}</Text>
+      <Button title="Увеличить" onPress={() => setCount(count + 1)} />
+      <FlatList
+        data={memoizedData}
+        renderItem={({ item }) => <MemoizedItem name={item.name} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 18,
-    marginVertical: 15,
-  },
-});
-
-export default Lab3;
+export default App;
