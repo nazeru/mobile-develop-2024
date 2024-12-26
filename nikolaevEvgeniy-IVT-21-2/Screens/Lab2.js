6c23a2997eef4a45e0cc0d17cf6b4c87d642f66c
L1ghtowo2803 <90662386+L1ghtowo2803@users.noreply.github.com>
2024-12-26 23:51:43 +0900

    lab2 исправление

--- nikolaevEvgeniy-IVT-21-2/Screens/Lab2.js
+++ nikolaevEvgeniy-IVT-21-2/Screens/Lab2.js
@@ -1,41 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { commonStyles } from "../styles";
 
 const Lab2 = () => {
-  const [name, setName] = useState("");
-  const [greeting, setGreeting] = useState("");
+  const [catImage, setCatImage] = useState("");
+
+  const fetchRandomCat = () => {
+    const randomCatUrl = `https://cataas.com/cat?timestamp=${new Date().getTime()}`;
+    setCatImage(randomCatUrl);
+  };
 
   useEffect(() => {
-    const newGreeting = name ? `Привет, ${name}!` : "Введите своё имя.";
-    setGreeting(newGreeting);
-
-    return () => {
-      console.log(`Очистка: имя было "${name}"`);
-    };
-  }, [name]);
+    fetchRandomCat();
+  }, []);
 
   return (
     <View style={commonStyles.container}>
       <Text style={commonStyles.title}>Lab2: useEffect</Text>
-      <TextInput
-        style={commonStyles.input}
-        placeholder="Введите своё имя"
-        value={name}
-        onChangeText={setName}
-      />
-      <Text style={styles.greeting}>{greeting}</Text>
+      <Text style={commonStyles.title1}>Random Cat</Text>
+      {catImage ? (
+        <Image source={{ uri: catImage }} style={commonStyles.image} />
+      ) : (
+        <Text>Loading...</Text>
+      )}
+      <TouchableOpacity style={commonStyles.button} onPress={fetchRandomCat}>
+        <Text style={commonStyles.buttonText}>Нового кота!</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  greeting: {
-    fontSize: 18,
-    marginVertical: 20,
-    color: "#007BFF",
-    fontWeight: "bold",
-  },
-});
-
 export default Lab2;
