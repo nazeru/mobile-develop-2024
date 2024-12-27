5e705ae2600263860bdab105ebff9daf54cf68e0
Ruslan32145 <tarasovr429@gmail.com>
2024-12-27 19:48:25 +0900

    небольшие изменения lab3

--- tarasovRuslan/screens/Lab3.js
+++ tarasovRuslan/screens/Lab3.js
@@ -2,12 +2,14 @@ import React, { useState, useMemo } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 const Lab3 = () => {
+
   const [number, setNumber] = useState(1);
   const [result, setResult] = useState(null);
   const [timeWithoutMemo, setTimeWithoutMemo] = useState(null); // Время без useMemo
   const [timeWithMemo, setTimeWithMemo] = useState(null); // Время с useMemo
 
   const calculateFactorial = (n) => {
+    console.log(`Вычисление факториала для n = ${n}`);
     let fact = 1;
     for (let i = 1; i <= n; i++) {
       fact *= i;
@@ -16,24 +18,31 @@ const Lab3 = () => {
   };
 
   const computeFactorialWithoutMemo = () => {
+    console.log('Вычисление факториала без useMemo...');
     const startTime = performance.now();
     const fact = calculateFactorial(number);
     const endTime = performance.now();
     const elapsedTime = (endTime - startTime).toFixed(3); // Время в миллисекундах
+    console.log(`Время выполнения без useMemo: ${elapsedTime} мс`);
     setTimeWithoutMemo(elapsedTime);
     setResult(fact);
   };
 
   const computeFactorialWithMemo = () => {
+    console.log('Вычисление факториала с useMemo...');
     const startTime = performance.now();
     const fact = memoizedFactorial;
     const endTime = performance.now();
     const elapsedTime = (endTime - startTime).toFixed(3); // Время в миллисекундах
+    console.log(`Время выполнения с useMemo: ${elapsedTime} мс`);
     setTimeWithMemo(elapsedTime);
     setResult(fact);
   };
 
-  const memoizedFactorial = useMemo(() => calculateFactorial(number), [number]);
+  const memoizedFactorial = useMemo(() => {
+    console.log(`Кэширование факториала для числа = ${number}`);
+    return calculateFactorial(number);
+  }, [number]);
 
   return (
     <View style={styles.container}>
@@ -49,8 +58,20 @@ const Lab3 = () => {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <Button title="Увеличить на 1" onPress={() => setNumber(number + 1)} />
-        <Button title="Увеличить на 100" onPress={() => setNumber(number + 100)} />
+        <Button
+          title="Увеличить на 1"
+          onPress={() => {
+            console.log('Увеличение числа на 1');
+            setNumber(number + 1);
+          }}
+        />
+        <Button
+          title="Увеличить на 100"
+          onPress={() => {
+            console.log('Увеличение числа на 100');
+            setNumber(number + 100);
+          }}
+        />
       </View>
 
       <View style={styles.buttonsContainer}>
